67417050a3f30c513e353d567ec5f9e2da15219a
joaquinsoza <joaquinsozag@gmail.com>
2023-07-05 13:46:07 -0400

    feat: new pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
+import React from 'react';
+import MainLayout from '../src/components/layout/MainLayout'
+import MySorobanReactProvider from '../src/soroban/MySorobanReactProvider'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { theme } from '../src/themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { ColorModeContext } from '../src/contexts';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mode, setMode] = React.useState<'light' | 'dark'>('dark');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme(mode)}>
+        <CssBaseline />
+        <MySorobanReactProvider>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        </MySorobanReactProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
---
+++ pages/liquidity.tsx
@@ -0,0 +1,12 @@
+import { ProvideLiquidity } from '../src/components/ProvideLiquidity';
+import SEO from '../src/components/SEO';
+
+export default function LiquidityPage() {
+
+  return (
+    <>
+      <SEO title='Liquidity - Soroswap' description='Soroswap Liquidity Pool' />
+      <ProvideLiquidity />
+    </>
+  )
+}
