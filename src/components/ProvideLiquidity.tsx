99caf344d62e40ce8220bf11452c7c88c63aa9b5
jtschuwirth <jtschuwirth@uc.cl>
2023-07-05 22:38:24 -0400

    feat/provide liquidity working

--- src/components/ProvideLiquidity.tsx
+++ src/components/ProvideLiquidity.tsx
@@ -14,10 +14,15 @@ import TokensDropdown from './TokensDropwndown';
 import {ProvideLiquidityButton} from './buttons/ProvideLiquidityButton';
 import { TokenType } from '../interfaces/tokens';
 import { usePairExist } from '../hooks/usePairExist';
-import { tokenBalance, useFactory } from '../hooks';
+import { useTokenBalance, useFactory } from '../hooks';
 import { usePairContractAddress } from '../hooks/usePairContractAddress';
+import { DepositButton } from './buttons/DepositButton';
+import BigNumber from 'bignumber.js';
+import { PairBalance } from './PairBalance';
 
 export function ProvideLiquidity (){
+    //Address pair hardcodeado se va a quitar!!
+    let pairAddress = "CAWMGGWVYFF3JY3BXGUPBQO7LAGGKNQEGZ6HULJ24WIUVCKHITIXKTM4"
     const sorobanContext=useSorobanReact()
     const tokens = useTokens(sorobanContext)
 
@@ -27,8 +32,7 @@ export function ProvideLiquidity (){
     const [inputTokenAmount, setInputTokenAmount] = React.useState(0);
     const [outputTokenAmount, setOutputTokenAmount] = React.useState(0);
 
-    const pairAddress = usePairContractAddress(inputToken?.token_address ?? "", outputToken?.token_address ?? "", sorobanContext)
-    //const pairBalance = tokenBalance(pairAddress ?? "", sorobanContext.address)
+    //const pairAddress = usePairContractAddress(inputToken?.token_address ?? "", outputToken?.token_address ?? "", sorobanContext)
 
     const handleInputTokenChange = (event: React.ChangeEvent<{ value: string}>) => {
       const token = tokens.find((token) => 
@@ -85,9 +89,9 @@ export function ProvideLiquidity (){
           />
         </FormControl>
         <p>.</p>
-        <TokensDropdown tokens={tokens} onChange={handleOutputTokenChange} title={"Output token"} pairExist={
-          (option: TokenType) => inputToken!=null?usePairExist(inputToken.token_address, option.token_address, sorobanContext):false
-        }/>
+        <TokensDropdown tokens={tokens} onChange={handleOutputTokenChange} title={"Output token"} 
+        //pairExist={(option: TokenType) => inputToken!=null?usePairExist(inputToken.token_address, option.token_address, sorobanContext):false}
+        />
         <FormControl>
           
         
@@ -104,16 +108,19 @@ export function ProvideLiquidity (){
           />
         </FormControl>
 
-        <p>Current pair address {pairAddress}</p>
-        <p>Current pair balance {}</p>
+        <p>Current pair address {}</p>
+        <p>Current pair balance</p>
+        {sorobanContext.address?<PairBalance pairAddress={pairAddress} sorobanContext={sorobanContext}/>:0}
         <p>Liquidity Pool tokens to receive: TODO</p>
 
       </CardContent>
       <CardActions>
-        <ProvideLiquidityButton
-            inputTokenAmount_1 = {inputTokenAmount}
-            inputTokenAmount_2 = {outputTokenAmount}
-        />
+        {inputToken!==null&&outputToken!==null?<DepositButton
+            pairAddress={pairAddress}
+            amount0={BigNumber(inputTokenAmount)}
+            amount1={BigNumber(outputTokenAmount)}
+            sorobanContext={sorobanContext}
+        />:null}
       </CardActions>
       
     </Card>
---
+++ src/components/buttons/DepositButton.tsx
@@ -0,0 +1,84 @@
+import {SorobanContextType} from '@soroban-react/core';
+import {contractTransaction, useContractValue, useSendTransaction} from '@soroban-react/contracts'
+import {
+    accountToScVal, bigNumberToI128,
+  } from '../../helpers/utils';
+import BigNumber from 'bignumber.js';
+import { useState } from 'react';
+import * as SorobanClient from 'soroban-client'
+import { Button } from '@mui/material';
+
+interface DepositButtonProps {
+  pairAddress:string, 
+  amount0:BigNumber, 
+  amount1:BigNumber, 
+  sorobanContext:SorobanContextType
+}
+
+export function DepositButton({
+  pairAddress, 
+  amount0, 
+  amount1, 
+  sorobanContext}:DepositButtonProps){
+    const [isSubmitting, setSubmitting] = useState(false)
+    const networkPassphrase = sorobanContext.activeChain?.networkPassphrase ?? ''
+    const server = sorobanContext.server
+    const account = sorobanContext.address
+    const { sendTransaction } = useSendTransaction()
+
+    const depositTokens = async () => {
+        setSubmitting(true)
+    
+        //Parse amount to mint to BigNumber and then to i128 scVal
+        const amount0ScVal = bigNumberToI128(amount0.shiftedBy(7))
+        const amount1ScVal = bigNumberToI128(amount1.shiftedBy(7))
+    
+        let walletSource
+        
+        try{
+          walletSource = await server?.getAccount(account)
+        } catch(error){
+          alert("Your wallet or the token admin wallet might not be funded")
+          setSubmitting(false)  
+          return
+        }
+          
+        const options = {
+          sorobanContext,
+        }
+    
+        try {
+          //Builds the transaction
+          let tx = contractTransaction({
+            source: walletSource,
+            networkPassphrase,
+            contractId: pairAddress,
+            method: 'deposit',
+            params: [new SorobanClient.Address(account).toScVal(), amount0ScVal, amount0ScVal, amount1ScVal, amount1ScVal]
+          })
+    
+          //Sends the transactions to the blockchain
+          let result = await sendTransaction(tx, options)
+          if (result){
+            alert("Success!")
+          }
+          console.log("🚀 ~ file: DepositButton.tsx ~ depositTokens ~ result:", result)
+          
+          
+    
+          //This will connect again the wallet to fetch its data
+          sorobanContext.connect()
+    
+        } catch (error) {
+          console.log("🚀 « error:", error)
+        }
+        
+        setSubmitting(false)
+      }
+
+      return (
+        <Button onClick={depositTokens} disabled={isSubmitting} >
+          Deposit Liquidity!
+        </Button>
+      )
+}
