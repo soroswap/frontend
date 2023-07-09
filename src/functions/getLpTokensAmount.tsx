2a9635eef8768af0f01d5c3450af0df8f51d100b
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-07-08 23:15:41 -0300

    fix(functions): use correct formula for LP to receive

--- src/functions/getLpTokensAmount.tsx
+++ src/functions/getLpTokensAmount.tsx
@@ -5,12 +5,16 @@ import { useTotalShares } from "../hooks/useTotalShares";
 export default function getLpTokensAmount(
   amount0: BigNumber,
   reserve0: BigNumber,
+  amount1: BigNumber,
+  reserve1: BigNumber,
   pairAddress: string,
   sorobanContext: SorobanContextType,
 ): BigNumber {
     const totalShares = useTotalShares(pairAddress, sorobanContext)
-    if (!totalShares || amount0.isEqualTo(0) || reserve0.isEqualTo(0)) {
-        return new BigNumber(0)
+    if (!totalShares || reserve1.isEqualTo(0) || reserve0.isEqualTo(0)) {
+        return amount0.multipliedBy(amount1).squareRoot()
     }
-    return amount0.dividedBy(reserve0).multipliedBy(totalShares).decimalPlaces(0);
+    let LP0=amount0.multipliedBy(totalShares).dividedBy(reserve0).decimalPlaces(0);
+    let LP1=amount1.multipliedBy(totalShares).dividedBy(reserve1).decimalPlaces(0);
+    return BigNumber.min(LP0, LP1)
 }
