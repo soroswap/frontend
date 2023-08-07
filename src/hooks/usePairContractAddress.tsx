61474a22be17a17a7f738c720947a6fc7d0ffdd9
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-08-07 13:29:05 -0400

    Better comments in usePairContractrAddress

--- src/hooks/usePairContractAddress.tsx
+++ src/hooks/usePairContractAddress.tsx
@@ -10,29 +10,31 @@ export function usePairContractAddress(
   address_1: string|null,
   sorobanContext: SorobanContextType,
 ): string | undefined {
-  console.log("🚀 « address_1:", address_1)
-  console.log("🚀 « address_0:", address_0)
+  const random = Math.random(); 
+  console.log("rando: ", random,"🚀 ~ file: usePairContractAddress.tsx:13 ~ address_1:", address_1)
+  console.log("rando: ", random,"🚀 ~ file: usePairContractAddress.tsx:13 ~ address_0:", address_0)
+  
   let pairAddress;
   let params: xdr.ScVal[] = [];
   if (address_0 !== null && address_1 !== null) {
     params = [accountToScVal(address_0), accountToScVal(address_1)];
   }
-  console.log("🚀 « pairAddress:", pairAddress)
   const factory = useFactory(sorobanContext);
-  console.log("🚀 « factory:", factory)
   const pairAddress_scval = useContractValue({
     contractId: factory.factory_address,
     method: "get_pair",
     params: params,
     sorobanContext: sorobanContext,
   });
-  console.log("🚀 « pairAddress_scval:", pairAddress_scval)
+  console.log("rando: ", random,"🚀 ~ file: usePairContractAddress.tsx:29 ~ pairAddress_scval:", pairAddress_scval)
 
   if (pairAddress_scval.result) {
     pairAddress = SorobanClient.Address.fromScVal(
       pairAddress_scval.result,
     ).toString();
+    console.log("rando: ", random,"🚀 ~ file: usePairContractAddress.tsx:36 ~ pairAddress:", pairAddress)
   } else return undefined;
-  console.log("🚀 « pairAddress:", pairAddress)
+  
+  console.log("rando: ", random,"🚀 ~ file: usePairContractAddress.tsx:36 ~ pairAddress:", pairAddress)
   return pairAddress;
 }
