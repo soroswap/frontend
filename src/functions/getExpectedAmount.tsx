61810468e516bd552c099080de6babdc1d95d837
joaquinsoza <joaquinsozag@gmail.com>
2023-09-07 21:05:20 +0200

    feat/ new functions for priceImpact, PairAddress, Factory, ExpectedAmount

---
+++ src/functions/getExpectedAmount.tsx
@@ -0,0 +1,39 @@
+import { SorobanContextType } from "@soroban-react/core"
+import { TokenType } from "interfaces"
+import { getPairAddress } from "./getPairAddress"
+import { reservesBNWithTokens } from "hooks/useReserves"
+import BigNumber from "bignumber.js"
+import { formatTokenAmount } from "helpers/format"
+import fromExactInputGetExpectedOutput from "./fromExactInputGetExpectedOutput"
+import fromExactOutputGetExpectedInput from "./fromExactOutputGetExpectedInput"
+
+interface customReservesType {
+  token0: TokenType
+  token1: TokenType
+  reserve0: BigNumber
+  reserve1: BigNumber
+}
+
+export async function getExpectedAmount(
+  currencyIn: TokenType | undefined,
+  currencyOut: TokenType | undefined,
+  amountIn: BigNumber,
+  sorobanContext: SorobanContextType,
+  customReserves?: customReservesType
+) {
+  if(!currencyIn || !currencyOut) return BigNumber("0")
+
+  const pairAddress = await getPairAddress(currencyIn.address, currencyOut.address, sorobanContext)
+  const reserves = customReserves ?? await reservesBNWithTokens(pairAddress, sorobanContext)
+
+  const isToken0 = currencyIn.address === reserves.token0
+
+  let expectedOutput
+  if (isToken0) {
+    expectedOutput = fromExactInputGetExpectedOutput(amountIn, reserves.reserve0, reserves.reserve1)
+  } else {
+    expectedOutput = fromExactOutputGetExpectedInput(amountIn, reserves.reserve0, reserves.reserve1)
+  }
+
+  return expectedOutput
+}
---
+++ src/functions/getPairAddress.tsx
@@ -0,0 +1,30 @@
+import { contractInvoke } from "@soroban-react/contracts";
+import { SorobanContextType } from "@soroban-react/core";
+import { getFactory } from "./getFactory";
+import { addressToScVal, scValStrToJs } from "helpers/convert";
+
+export async function getPairAddress(
+  address_0: string | undefined,
+  address_1: string | undefined,
+  sorobanContext: SorobanContextType
+) {
+  if(!address_0 || !address_1) return ""
+  const factory = await getFactory(sorobanContext)
+    
+  const response = await contractInvoke({
+    contractAddress: factory.factory_address,
+    method: "get_pair",
+    args: [
+      addressToScVal(address_0),
+      addressToScVal(address_1),
+    ],
+    sorobanContext,
+  })
+
+  if (response) {
+    const pairAddress = scValStrToJs(response.xdr) as string
+    return pairAddress
+  } else {
+    return ""
+  }
+}
