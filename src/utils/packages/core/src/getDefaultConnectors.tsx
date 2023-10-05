a0dc0f4c29a804b33927fb2ee209b99a7f022b70
joaquinsoza <joaquinsozag@gmail.com>
2023-10-05 16:12:03 +0200

    removed soroban-react local library

--- src/utils/packages/core/src/getDefaultConnectors.tsx
+++
@@ -1,8 +0,0 @@
-import { freighter } from '../../freighter/src'
-import { Connector } from '../../types/src'
-
-export const getDefaultConnectors = (): Connector[] => {
-  const list: Connector[] = [freighter()]
-
-  return list
-}
