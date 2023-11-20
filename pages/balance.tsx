0543a96f1fcb8d238d28613b37a99e20cc20a9b1
maximiliano carrasco <maxxii.1820@gmail.com>
2023-11-20 17:28:03 -0300

    #202 #203 #204

---
+++ pages/balance.tsx
@@ -0,0 +1,11 @@
+import { Balances } from '../src/components/Balances';
+import SEO from '../src/components/SEO';
+
+export default function BalancesPage() {
+  return (
+    <>
+      <SEO title="Balances - Soroswap" description="Soroswap Balances" />
+      <Balances />
+    </>
+  );
+}
