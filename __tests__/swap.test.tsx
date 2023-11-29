d450a3994788123a15c1df48056e3dce91d1e574
Matias Poblete <matias.pobleteduran@gmail.com>
2023-11-29 17:46:51 -0300

    ✅ Created tests to validate the existence of Swap page components and added test tags to tested components

--- __tests__/swap.test.tsx
+++ __tests__/swap.test.tsx
@@ -1,22 +1,34 @@
 import React from "react";
-import { expect, test, it, describe } from 'vitest'
-import {screen, render} from '@testing-library/react'
-
-import { SwapComponent } from '../src/components/Swap/SwapComponent';
-import MySorobanReactProvider from "soroban/MySorobanReactProvider";
+import { Provider } from 'react-redux';
 import { ThemeProvider } from "@mui/material";
-import { theme } from '../src/themes';
-import SwapPage from "../pages";
+import {screen, render} from '@testing-library/react'
+import { expect, test, it, describe } from 'vitest'
 
-describe('swap Page', ()=>{
-    test('should render the page', ()=>{
-        render(
+import MySorobanReactProvider from "soroban/MySorobanReactProvider";
+import store from '../src/state'
+import { theme } from '../src/themes';
+import { SwapComponent } from '../src/components/Swap/SwapComponent';
+
+describe('Swap Page GUI', ()=>{
+    render(
+        <Provider store={store}>
             <ThemeProvider theme={theme('dark')}>
                 <MySorobanReactProvider>
-                    <SwapPage/>
+                    <SwapComponent/>
                 </MySorobanReactProvider>
             </ThemeProvider>
-        )
-
+        </Provider>
+    )
+    test('The container panel exists', ()=>{
+        const panel = screen.getByTestId('Swap__panel')
+        expect(panel).toBeDefined()
+    })
+    test('There are 2 token selectors', ()=>{
+        const token_selectors = screen.getAllByTestId('Swap__Panel__Selector')
+        expect(token_selectors.length).toBe(2)
+    })
+    test('The connect wallet button exists', ()=>{
+        const connect_button = screen.getByText('Connect Wallet')
+        expect(connect_button).toBeDefined()
     })
 })
