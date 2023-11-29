41911a08696d1b2b9876a9b51a81ff803533ae48
Matias Poblete <matias.pobleteduran@gmail.com>
2023-11-29 19:12:30 -0300

    🧹 Removed unnecessary imports on tests

--- __tests__/pages/balance.test.tsx
+++ __tests__/pages/balance.test.tsx
@@ -6,26 +6,18 @@ import { expect, test, it, describe } from 'vitest'
 import MySorobanReactProvider from "soroban/MySorobanReactProvider";
 import store from '../../src/state'
 import { theme } from '../../src/themes';
-import { SwapComponent } from '../../src/components/Swap/SwapComponent';
+import { Balances } from "components/Balances";
 
-describe('Swap Page GUI', ()=>{
+describe('Balance Page GUI', ()=>{
     render(
         <Provider store={store}>
             <ThemeProvider theme={theme('dark')}>
                 <MySorobanReactProvider>
-                    <SwapComponent/>
+                    <Balances/>
                 </MySorobanReactProvider>
             </ThemeProvider>
         </Provider>
     )
-    test('The container panel exists', ()=>{
-        const panel = screen.getByTestId('Swap__panel')
-        expect(panel).toBeDefined()
-    })
-    test('There are 2 token selectors', ()=>{
-        const token_selectors = screen.getAllByTestId('Swap__Panel__Selector')
-        expect(token_selectors.length).toBe(2)
-    })
     test('The connect wallet button exists', ()=>{
         const connect_button = screen.getByText('Connect Wallet')
         expect(connect_button).toBeDefined()
