a53f830b2eaea9b6e2ec5d2fb2dad58736bb6e5f
Matias Poblete <matias.pobleteduran@gmail.com>
2023-12-28 19:40:37 -0300

    Fix dependencies and update test configuration

--- src/components/Layout/Header.test.tsx
+++ src/components/Layout/Header.test.tsx
@@ -7,21 +7,30 @@ import { theme } from '../../themes';
 import Header from 'components/Layout/Header';
 
 describe('Header component', ()=>{
+    beforeEach(()=>{
     render(
         <ThemeProvider theme={theme('dark')}>
             <MySorobanReactProvider>
                 <Header isDrawerOpen={false} setDrawerOpen={()=>{}}/>
             </MySorobanReactProvider>
         </ThemeProvider>
-    )
+    )})
     
-    test('The navbar is rendered', ()=>{
-        const navbar = screen.getByTestId('nav')
+    test('The navbar is rendered', async ()=>{
+        const navbar = await screen.findByTestId('nav')
         expect(navbar).toBeDefined()
     })
-    const expected_links = ['balance', 'swap', 'liquidity']
-    test('All the navbar links are rendered', ()=>{
-        const links = screen.getAllByTestId('nav-link')
+    test('All the navbar links are rendered', async ()=>{
+        const expected_links = ['Balance', 'Swap', 'Liquidity']
+        const links = []
+        for (const link in expected_links) {
+            const element = expected_links[link]
+            const exists = await screen.findByText(element)
+            if(exists){
+                links.push(exists)
+            }
+        }
+
         expect(links.length).toBe(expected_links.length)
     })
 })
