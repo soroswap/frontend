88545ab04029a60baef9a152d40f6500b7e5338b
coderipper <hello@joaquinsoza.dev>
2024-01-15 13:58:11 +0100

    fix: useGetBalances to use all tokens on userlist and api

--- src/hooks/tokens/useAllTokens.ts
+++ src/hooks/tokens/useAllTokens.ts
@@ -3,11 +3,12 @@ import { useUserAddedTokens } from './useUserAddedTokens';
 
 //Returns tokens from the API and user added
 export const useAllTokens = () => {
-  const { tokensAsMap: apiTokensAsMap, tokens: apiTokens } = useApiTokens();
+  const { tokensAsMap: apiTokensAsMap, tokens: apiTokens, isLoading } = useApiTokens();
   const { tokensAsMap: userAddedTokensAsMap, tokens: userTokens } = useUserAddedTokens();
 
   return {
     tokensAsMap: { ...apiTokensAsMap, ...userAddedTokensAsMap },
     tokens: [...apiTokens, ...userTokens],
+    isLoading: isLoading,
   };
 };
