645f5384d415cba90820f82e95598044b7dabbc9
Matias Poblete <matias.pobleteduran@gmail.com>
2024-02-21 05:52:57 -0300

    🚧xBull wallet support - Modified WalletButton component and created ConnectorsDropdown

--- src/components/Buttons/WalletButton.tsx
+++ src/components/Buttons/WalletButton.tsx
@@ -2,8 +2,11 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { useSorobanReact } from '@soroban-react/core';
 import { ButtonPrimary, ButtonLight } from 'components/Buttons/Button';
+import { ConnectorsDropdown } from 'components/Buttons/ConnectorsDropdown';
 import { AppContext } from 'contexts';
 import { isConnected, isAllowed, setAllowed } from "@stellar/freighter-api";
+import { xbull } from '@soroban-react/xbull';
+import { freighter } from '@soroban-react/freighter';
 
 import * as Bowser from 'bowser'
 
@@ -13,6 +16,7 @@ export function WalletButton({ style, light }: { style?: React.CSSProperties; li
     const { ConnectWalletModal } = useContext(AppContext);
     const { isConnectWalletModalOpen, setConnectWalletModalOpen } = ConnectWalletModal;
     const [ hasFreighter, setHasFreighter ]  = useState(false)
+    const [ hasXBull, setHasXBull ]  = useState(false)
     const handleClick = () => {
         setConnectWalletModalOpen(true);
     };
@@ -45,22 +49,35 @@ export function WalletButton({ style, light }: { style?: React.CSSProperties; li
         return connected
       }
     const ButtonComponent = light ? ButtonLight : ButtonPrimary;
-      
+    //console.log(sorobanContext.activeConnector)
+    
     useEffect(() => {
+        if((window as any).xBullSDK){
+            console.log('xBullSDK is available');
+          } else {
+            console.log('xBullSDK is not available');
+          }
         connected();
     }, [])
     
     return (
-        <>
+        <>  
+
             { hasFreighter ? (
-                <ButtonComponent style={style} onClick={handleClick}>
-                    Connect Wallet
-                </ButtonComponent>
-            ) : (
-                <ButtonComponent style={style} onClick={installFreighter}>
-                    Install Freighter
-                </ButtonComponent>
-            )
+                    <>
+                   {/*      <ButtonComponent style={style} onClick={handleClick}>
+                            Connect Wallet
+                        </ButtonComponent> */}
+                        <ConnectorsDropdown/>
+                    </>
+                ) : (
+                    <>
+           {/*              <ButtonComponent style={style} onClick={installFreighter}>
+                            Install Freighter
+                        </ButtonComponent> */}
+                        <ConnectorsDropdown/>
+                    </>
+                )   
             }
             
         </>
