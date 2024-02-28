1b952fcef8de790b136476b82c4dc8bc31f78ab5
abstract829 <maxxii.1820@gmail.com>
2024-02-28 16:39:27 -0300

    changes on router sdk to make swap work when backend fails

--- src/functions/generateRoute.ts
+++ src/functions/generateRoute.ts
@@ -1,3 +1,4 @@
+import { useSorobanReact } from '@soroban-react/core';
 import {
   ChainId,
   CurrencyAmount,
@@ -6,11 +7,11 @@ import {
   TradeType as SdkTradeType,
   Protocols,
 } from 'soroswap-router-sdk';
+import { useFactory } from 'hooks';
+import { useMemo } from 'react';
 
 const backendUrl = process.env.NEXT_PUBLIC_SOROSWAP_BACKEND_URL;
 const apiKey = process.env.NEXT_PUBLIC_SOROSWAP_BACKEND_API_KEY;
-
-const router = new Router(backendUrl!, apiKey!, 60, [Protocols.SOROSWAP]);
 
 export interface GenerateRouteProps {
   amountTokenAddress: string;
@@ -19,33 +20,59 @@ export interface GenerateRouteProps {
   tradeType: SdkTradeType;
 }
 
-const fromAddressToToken = (address: string) => {
-  return new Token(ChainId.TESTNET, address, 18);
+const chainNameToId: { [x: string]: number } = {
+  testnet: ChainId.TESTNET,
+  standalone: ChainId.STANDALONE,
+  futurenet: ChainId.FUTURENET,
 };
 
-const fromAddressAndAmountToCurrencyAmount = (address: string, amount: string) => {
-  const token = fromAddressToToken(address);
-  return CurrencyAmount.fromRawAmount(token, amount);
-};
+export const useRouterSDK = () => {
+  const sorobanContext = useSorobanReact();
+  const { factory } = useFactory(sorobanContext);
 
-export const resetRouterSdkCache = () => {
-  router.resetCache();
-};
+  const router = useMemo(() => {
+    if (!backendUrl || !apiKey) {
+      throw new Error(
+        'NEXT_PUBLIC_SOROSWAP_BACKEND_URL and NEXT_PUBLIC_SOROSWAP_BACKEND_API_KEY must be set in the environment variables.',
+      );
+    }
+    const chainId = chainNameToId[sorobanContext.activeChain?.name ?? 'testnet'];
 
-export const generateRoute = async ({
-  amountTokenAddress,
-  quoteTokenAddress,
-  amount,
-  tradeType,
-}: GenerateRouteProps) => {
-  if (!backendUrl || !apiKey) {
-    throw new Error(
-      'NEXT_PUBLIC_SOROSWAP_BACKEND_URL and NEXT_PUBLIC_SOROSWAP_BACKEND_API_KEY must be set in the environment variables.',
+    return new Router(backendUrl, apiKey, 60, [Protocols.SOROSWAP], chainId);
+  }, [sorobanContext.activeChain?.name]);
+
+  const fromAddressToToken = (address: string) => {
+    return new Token(ChainId.TESTNET, address, 18);
+  };
+
+  const fromAddressAndAmountToCurrencyAmount = (address: string, amount: string) => {
+    const token = fromAddressToToken(address);
+    return CurrencyAmount.fromRawAmount(token, amount);
+  };
+
+  const resetRouterSdkCache = () => {
+    router.resetCache();
+  };
+
+  const generateRoute = async ({
+    amountTokenAddress,
+    quoteTokenAddress,
+    amount,
+    tradeType,
+  }: GenerateRouteProps) => {
+    if (!factory.factory_address) throw new Error('Factory address not found');
+
+    const currencyAmount = fromAddressAndAmountToCurrencyAmount(amountTokenAddress, amount);
+    const quoteCurrency = fromAddressToToken(quoteTokenAddress);
+
+    return router.route(
+      currencyAmount,
+      quoteCurrency,
+      tradeType,
+      factory.factory_address,
+      sorobanContext,
     );
-  }
+  };
 
-  const currencyAmount = fromAddressAndAmountToCurrencyAmount(amountTokenAddress, amount);
-  const quoteCurrency = fromAddressToToken(quoteTokenAddress);
-
-  return router.route(currencyAmount, quoteCurrency, tradeType);
+  return { generateRoute, resetRouterSdkCache };
 };
