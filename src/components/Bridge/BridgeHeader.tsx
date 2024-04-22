7148b25f922e09217f3494fa100464a1cae3234a
Matias Poblete <matias.pobleteduran@gmail.com>
2024-04-22 18:19:01 -0400

    ♻️Refactor bridge page to show user info in main component

--- src/components/Bridge/BridgeHeader.tsx
+++ src/components/Bridge/BridgeHeader.tsx
@@ -1,10 +1,13 @@
 // import { Trans } from '@lingui/macro' //This is for localization and translation on all languages
+import { useBalance, useInkathon } from '@scio-labs/use-inkathon';
+import { shortenAddress } from 'helpers/address';
 import { styled, useTheme } from '@mui/material/styles';
+import { Grid, useMediaQuery } from '@mui/material';
+import { HeaderChip } from 'components/Layout/ProfileSection';
 import { RowBetween, RowFixed } from '../Row';
 import { SubHeader } from '../Text';
-import TuneRoundedIcon from '@mui/icons-material/TuneRounded';
-import SettingsTab from '../Settings/index';
-import { useMediaQuery } from '@mui/material';
+import { useSorobanReact } from '@soroban-react/core';
+import { ArrowDropDownSharp } from '@mui/icons-material';
 const StyledBridgeHeader = styled(RowBetween)(({ theme }) => ({
   marginBottom: 10,
   color: theme.palette.secondary.main,
@@ -24,15 +27,54 @@ export default function BridgeHeader({
   chainId?: number;
   trade?: boolean;
 }) {
+  const { isConnected, activeAccount, error, activeChain, isConnecting, disconnect } = useInkathon();
+  const { address } = useSorobanReact();
+  const { balanceFormatted,   } = useBalance(activeAccount?.address, true);
   const theme = useTheme();
   const fiatOnRampButtonEnabled = true;
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const paddingX = {padding: '0px 12px'}
+  const showBalances = () => {
+    console.log(balanceFormatted);
+  }
 
   return (
     <StyledBridgeHeader>
-      <HeaderButtonContainer>
-        <SubHeader fontSize={isMobile ? 14 : undefined}>Bridge</SubHeader>
-      </HeaderButtonContainer>
+      {/* <Button onClick={()=>showBalances()}>Show balances</Button> */}
+      <Grid container>
+        <Grid item xs={4}>
+          <HeaderButtonContainer>
+            <SubHeader fontSize={isMobile ? 14 : undefined}>Bridge</SubHeader>
+          </HeaderButtonContainer>
+        </Grid>
+        {activeAccount && activeChain && address && (
+          <>
+            <Grid item xs={8} alignContent={'center'}>
+              <Grid container columnSpacing={2}>
+                <Grid item xs={5}>
+                  <HeaderChip label={activeChain?.name} isSmall/>
+                </Grid>
+                <Grid item xs={7} style={paddingX}>
+                  <HeaderChip label={
+                      <>
+                        {shortenAddress(activeAccount?.address!)}
+                        <ArrowDropDownSharp key={'action-icon'} className='MuiChip-action-icon'/>
+                      </>
+                    } canDisconnect isSmall disconnect={()=>disconnect!()}/>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item xs={6} style={paddingX}>
+                Native asset balance:
+              </Grid>
+              <Grid item xs={6} textAlign={'end'} style={paddingX}>
+                {balanceFormatted}
+              </Grid>
+            </Grid>
+          </>
+        )}
+      </Grid>
     </StyledBridgeHeader>
   );
 }
