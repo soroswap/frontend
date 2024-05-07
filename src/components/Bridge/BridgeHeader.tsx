e0d1ed07bb6dff3c9ee42089d01183dad5f941d5
Matias Poblete <matias.pobleteduran@gmail.com>
2024-05-07 01:55:02 -0400

    ✨ Add chain address to BridgeHeader component & UI improvements

--- src/components/Bridge/BridgeHeader.tsx
+++ src/components/Bridge/BridgeHeader.tsx
@@ -2,12 +2,17 @@
 import { useBalance, useInkathon } from '@scio-labs/use-inkathon';
 import { shortenAddress } from 'helpers/address';
 import { styled, useTheme } from '@mui/material/styles';
-import { Grid, useMediaQuery } from '@mui/material';
+import { Button, Grid, Typography, useMediaQuery, Tooltip } from '@mui/material';
 import { HeaderChip } from 'components/Layout/ProfileSection';
 import { RowBetween, RowFixed } from '../Row';
 import { SubHeader } from '../Text';
 import { useSorobanReact } from '@soroban-react/core';
 import { ArrowDropDownSharp } from '@mui/icons-material';
+import useGetNativeTokenBalance from 'hooks/useGetNativeTokenBalance';
+import BigNumber from 'bignumber.js';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import useNotification from 'hooks/useNotification';
+import { SnackbarIconType } from 'contexts';
 const StyledBridgeHeader = styled(RowBetween)(({ theme }) => ({
   marginBottom: 10,
   color: theme.palette.secondary.main,
@@ -18,60 +23,113 @@ const HeaderButtonContainer = styled(RowFixed)`
   gap: 16px;
 `;
 
-export default function BridgeHeader({
-  autoSlippage,
-  chainId,
-  trade,
-}: {
-  autoSlippage?: number;
-  chainId?: number;
-  trade?: boolean;
-}) {
+interface Chains {
+  name: string;
+  address: string;
+  balance: string;
+}
+
+const HeaderChainData = (props: any)=> {
+  const chains: Chains[] = props.chains;
+  const { notify } = useNotification();
+
+  const Wrapper = styled(Grid)`
+  background: ${({ theme }) => theme.palette.background.default};
+  border-radius: 16px;
+  padding: 12px 16px;
+  overflow: hidden;
+`;
+
+  return (
+    <Wrapper container>
+      <Grid container xs={12} >
+        <Grid item xs={4} >
+          <Typography color={'#fdfdfe'} variant={'body2'}>
+            Chain name
+          </Typography>
+        </Grid>
+        <Grid item xs={4} >
+          <Typography color={'#fdfdfe'} textAlign={'center'} variant={'body2'}>
+            Address
+          </Typography>
+        </Grid>
+        <Grid item xs={4} >
+          <Typography color={'#fdfdfe'} textAlign={'end'} variant={'body2'}>
+            Balance
+          </Typography>
+        </Grid>
+      </Grid>
+      {chains.map((chain: Chains, index: number) => (
+        <>
+          <Grid item xs={4} >
+            <Typography variant={'caption'}>
+              {chain.name}
+            </Typography>
+          </Grid>
+          <Grid item xs={4} textAlign={'center'}>
+            <CopyToClipboard text={chain.address} onCopy={() =>
+                  notify?.({
+                    message: `${chain.name} address copied to clipboard!`,
+                    title: 'Copied',
+                    type: SnackbarIconType.MINT,
+                  })}>
+              <Tooltip title={chain.address} followCursor arrow>
+                <Button size="small" variant="text">
+                    {shortenAddress(chain.address)}
+                </Button>
+              </Tooltip>
+            </CopyToClipboard>
+          </Grid>
+          <Grid item xs={4} textAlign={'end'}>
+            <Typography variant={'caption'}>
+              {chain.balance}
+            </Typography>
+          </Grid>
+        </>
+      ))}
+      </Wrapper>
+  )
+}
+
+export default function BridgeHeader() {
   const { isConnected, activeAccount, error, activeChain, isConnecting, disconnect } = useInkathon();
   const { address } = useSorobanReact();
-  const { balanceFormatted,   } = useBalance(activeAccount?.address, true);
+  const { data } = useGetNativeTokenBalance();
+
+  const formattedNativeBalance = () => {
+    const rawAmount = data?.data;
+    const parsedAmount = new BigNumber(rawAmount!).dividedBy(10 ** 7).toFixed(7);
+    return `${parsedAmount} XLM`;
+  }
+  
+  const { balanceFormatted } = useBalance(activeAccount?.address, true);
   const theme = useTheme();
   const fiatOnRampButtonEnabled = true;
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const paddingX = {padding: '0px 12px'}
-  const showBalances = () => {
-    console.log(balanceFormatted);
-  }
+  const chainsInfo = [
+    {
+      name: activeChain?.name,
+      address: activeAccount?.address,
+      balance: balanceFormatted
+    },
+    {
+      name: 'Stellar',
+      address: address,
+      balance: formattedNativeBalance()
+    }
+  ]
 
   return (
     <StyledBridgeHeader>
-      {/* <Button onClick={()=>showBalances()}>Show balances</Button> */}
-      <Grid container>
-        <Grid item xs={4}>
+      <Grid container  rowSpacing={2}>
+        <Grid item xs={12}>
           <HeaderButtonContainer>
             <SubHeader fontSize={isMobile ? 14 : undefined}>Bridge</SubHeader>
           </HeaderButtonContainer>
         </Grid>
         {activeAccount && activeChain && address && (
           <>
-            <Grid item xs={8} alignContent={'center'}>
-              <Grid container columnSpacing={2}>
-                <Grid item xs={5}>
-                  <HeaderChip label={activeChain?.name} isSmall/>
-                </Grid>
-                <Grid item xs={7} style={paddingX}>
-                  <HeaderChip label={
-                      <>
-                        {shortenAddress(activeAccount?.address!)}
-                        <ArrowDropDownSharp key={'action-icon'} className='MuiChip-action-icon'/>
-                      </>
-                    } canDisconnect isSmall disconnect={()=>disconnect!()}/>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={6} style={paddingX}>
-                Native asset balance:
-              </Grid>
-              <Grid item xs={6} textAlign={'end'} style={paddingX}>
-                {balanceFormatted}
-              </Grid>
-            </Grid>
+            <HeaderChainData chains={[...chainsInfo] }/>
           </>
         )}
       </Grid>
