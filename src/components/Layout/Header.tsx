fd13bdc769d1031a845db2ee848584a6d01b00f7
abstract829 <maxxii.1820@gmail.com>
2024-07-02 10:25:15 -0400

    fix tests

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -98,7 +98,7 @@ const HeaderNavbar = () => {
     <Navbar
       onClickItem={(item) => {
         if (item.label === 'Info') {
-          window.open(`https://info.soroswap.finance`, '_blank');
+          window.open(`https://info.soroswap.finance`, '_self');
         } else {
           router.push(item.path);
         }
