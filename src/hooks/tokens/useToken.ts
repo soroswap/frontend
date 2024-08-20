f2c6987df631a2a64dd0d1bf57af56d955774f7c
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-08-20 07:58:46 -0400

    Add asset issuer for not listed tokens so trustline can be created

--- src/hooks/tokens/useToken.ts
+++ src/hooks/tokens/useToken.ts
@@ -16,19 +16,45 @@ export const findToken = async (
   if (!tokenAddress || tokenAddress === '') return undefined;
 
   const classicAssetSearch = getClassicAssetSorobanAddress(tokenAddress!, sorobanContext);
-
+  
   const formattedAddress = isAddress(classicAssetSearch ? classicAssetSearch : tokenAddress);
   if (!formattedAddress) return undefined;
-
+  
   const fromMap = tokensAsMap[formattedAddress];
-
+  
   if (fromMap) return fromMap;
-
+  
   const token = await getToken(sorobanContext, formattedAddress);
-
+  // const token: TokenType = {
+  //   contract: formattedAddress,
+  //   name: name as string,
+  //   code: symbol as string,
+  //   decimals,
+  //   icon: logo,
+  // };
+  console.log("🚀 ~ token, findToken, classicAssetSearch, formattedAddress:", token, classicAssetSearch, formattedAddress)
+  
   if (!token?.name || !token?.code) return undefined;
+  // Here from token.name we will try to understand if this is a classic asset (even if we got a soroban contracta as address).
+  const stellarAsset = getClassicStellarAsset(token.name);
 
-  return token;
+  /*
+    TODO: Here we might have a situation where a Soroban Contract has as name a CODE:ISSUER
+    We need to have a beter way to know if a soroban contract its for sure a stellar classic asset, and which.
+  */
+  if (stellarAsset && typeof stellarAsset !== 'boolean') {
+    return {
+      issuer: stellarAsset.issuer,
+      contract: token.contract,
+      name: stellarAsset.asset,
+      code: stellarAsset.assetCode,
+      decimals: 7,
+      icon: '',
+    };
+  }
+  else {
+    return token
+  };
 };
 
 const revalidateKeysCondition = (key: any) => {
@@ -54,6 +80,7 @@ export function useToken(tokenAddress: string | undefined) {
       findToken(tokenAddress, tokensAsMap, sorobanContext),
   );
 
+  console.log("findToken returned ", data)
   const handleTokenRefresh = () => {
     mutate((key: any) => revalidateKeysCondition(key), undefined, { revalidate: true });
   };
@@ -90,6 +117,8 @@ export function useToken(tokenAddress: string | undefined) {
       }
     }
   }
+  console.log("🚀 ~ useToken ~ newTokenData:", newTokenData)
+  console.log("🚀 ~ useToken ~ data:", data)
 
   //if not data and AssetExists return isWrapped: false
   return {
