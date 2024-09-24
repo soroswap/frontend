4d012fa62bc57c1be3b8089856369d621cc5be68
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-09-24 19:50:36 -0300

    ♻️Refactor useAggregator to toggle aggregator by network

--- src/hooks/useAggregator.tsx
+++ src/hooks/useAggregator.tsx
@@ -1,8 +1,9 @@
 import { SorobanContextType, useSorobanReact } from '@soroban-react/core';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 
-const shouldUseAggregator = process.env.NEXT_PUBLIC_AGGREGATOR_ENABLED === 'true';
+const aggregatorMainnet = process.env.NEXT_PUBLIC_AGGREGATOR_ENABLED_MAINNET === 'true';
+const aggregatorTestnet = process.env.NEXT_PUBLIC_AGGREGATOR_ENABLED_TESTNET === 'true';
 
 export const useAggregator = () => {
   const sorobanContext: SorobanContextType = useSorobanReact();
@@ -10,26 +11,29 @@ export const useAggregator = () => {
   const { activeChain } = sorobanContext;
 
   const [address, setAddress] = useState<string>();
-  const [isEnabled, setIsAggregatorEnabled] = useState<boolean>(false);
 
+  const shouldUseAggregator = useMemo(() => {
+    if (activeChain?.id === 'mainnet') {
+      return !!aggregatorMainnet
+    } else if (activeChain?.id === 'testnet') {
+      return !!aggregatorTestnet
+    }
+  }, [activeChain?.id])
 
-  useEffect(() => {
-    const setAggregatorData = async () => {
-      if (!sorobanContext) return;
-      const { data } = await axios.get(
-        `https://raw.githubusercontent.com/soroswap/aggregator/refs/heads/main/public/${activeChain?.id}.contracts.json`
-      ).catch((error) => {
-        console.error('Error fetching aggregator data', error);
-        console.warn('No address found Aggregator is disabled');
-        setIsAggregatorEnabled(false);
-        return { data: { ids: { aggregator: '' } } };
-      });
-      const aggregatorAddress = data.ids.aggregator;
-      setAddress(aggregatorAddress);
-      setIsAggregatorEnabled(shouldUseAggregator && !!aggregatorAddress);
-    };
-    setAggregatorData();
-  }, [activeChain?.id, sorobanContext]);
+  const isEnabled = useMemo(async () => {
+    if (!sorobanContext) return;
+    const { data } = await axios.get(
+      `https://raw.githubusercontent.com/soroswap/aggregator/refs/heads/main/public/${activeChain?.id}.contracts.json`
+    ).catch((error) => {
+      console.error('Error fetching aggregator data', error);
+      console.warn('No address found Aggregator is disabled');
+      return { data: { ids: { aggregator: '' } } };
+    });
+    const aggregatorAddress = data.ids.aggregator;
+    setAddress(aggregatorAddress);
+    const isEnabled = !!shouldUseAggregator && !!aggregatorAddress;
+    return isEnabled;
+  }, [activeChain?.id, shouldUseAggregator])
 
   return { address, isEnabled };
 };
