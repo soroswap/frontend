30689c5aaede53db80b3716afb0007209b9d0adf
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-04-01 16:29:16 -0300

    🔥Removed unused code

--- src/soroban/MySorobanReactProvider.tsx
+++ src/soroban/MySorobanReactProvider.tsx
@@ -3,20 +3,6 @@ import { ChainMetadata, WalletChain } from '@soroban-react/types';
 import { standalone, testnet, mainnet } from '@soroban-react/chains';
 import useMounted from 'hooks/useMounted';
 
-// Set allowed chains:
-const chains: ChainMetadata[] =
-  process.env.NODE_ENV === 'production' ? [testnet, mainnet] : [standalone, testnet, mainnet];
-
-// Set chain by default:
-// Helper function
-const findWalletChainByName = (name: string): WalletChain | undefined => {
-  return chains.find((chain) => chain.id === name);
-};
-
-// Get the active chain based on the environment variable or default to testnet
-const activeChainName = process.env.NEXT_PUBLIC_DEFAULT_NETWORK || 'testnet';
-const activeChain: WalletChain = findWalletChainByName(activeChainName) || testnet;
-
 const mainnetNetworkDetails: NetworkDetails = {
   network: WalletNetwork.PUBLIC,
   sorobanRpcUrl: 'https://soroban-rpc.creit.tech/',
