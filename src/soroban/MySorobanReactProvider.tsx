534e7912adba05b4b7d10cba486bc3a5fef5063d
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-04-01 16:39:32 -0300

    🩹Fix provider building

--- src/soroban/MySorobanReactProvider.tsx
+++ src/soroban/MySorobanReactProvider.tsx
@@ -1,14 +1,21 @@
 import { NetworkDetails, SorobanReactProvider, WalletNetwork } from 'stellar-react';
 import useMounted from 'hooks/useMounted';
 
+const WalletNetworks = [
+  "Public Global Stellar Network ; September 2015",
+  "Test SDF Network ; September 2015",
+  "Test SDF Future Network ; October 2022",
+  "Local Sandbox Stellar Network ; September 2022",
+  "Standalone Network ; February 2017"
+]
 const mainnetNetworkDetails: NetworkDetails = {
-  network: WalletNetwork.PUBLIC,
+  network: WalletNetworks[0] as WalletNetwork,
   sorobanRpcUrl: 'https://soroban-rpc.creit.tech/',
   horizonRpcUrl: 'https://horizon.stellar.org'
 }
 
 const testnetNetworkDetails: NetworkDetails = {
-  network: WalletNetwork.TESTNET,
+  network: WalletNetworks[1] as WalletNetwork,
   sorobanRpcUrl: 'https://soroban-testnet.stellar.org/',
   horizonRpcUrl: 'https://horizon-testnet.stellar.org'
 }
