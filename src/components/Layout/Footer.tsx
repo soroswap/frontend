c3b7826baa1914286c9af82bc13949e2d9759c23
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-04-02 05:43:29 -0300

    🩹Fixed attributes definition

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -104,7 +104,7 @@ const Footer: React.FC<FooterProps> = ({ isMobile }) => {
         </SocialButton>
         
         <SocialButton onClick={() => window.open('https://dune.com/paltalabs/soroswap', '_blank')}>
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="sc-bAEjGW fDEMCB"><path d="M11.993 23.981c6.623 0 11.993-5.368 11.993-11.99C23.986 5.368 18.616 0 11.993 0S.001 5.368.001 11.99c0 6.623 5.37 11.991 11.992 11.991z" fill="#F4603E"></path><path d="M2.088 18.754s7.924-2.596 21.891-7.15c0 0 .765 7.458-6.966 11.348 0 0-3.813 1.828-7.996.655 0 0-4.165-.786-6.93-4.853z" fill="#1E1870"></path></svg>
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="sc-bAEjGW fDEMCB"><path d="M11.993 23.981c6.623 0 11.993-5.368 11.993-11.99C23.986 5.368 18.616 0 11.993 0S.001 5.368.001 11.99c0 6.623 5.37 11.991 11.992 11.991z" fill="#F4603E"></path><path d="M2.088 18.754s7.924-2.596 21.891-7.15c0 0 .765 7.458-6.966 11.348 0 0-3.813 1.828-7.996.655 0 0-4.165-.786-6.93-4.853z" fill="#1E1870"></path></svg>
             Dune
         </SocialButton>
 
@@ -125,7 +125,7 @@ const Footer: React.FC<FooterProps> = ({ isMobile }) => {
         </SocialButton>
         <SocialButton onClick={() => window.open("https://medium.com/soroswap", '_blank')}>
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0,0,256,256">
-            <g fill="#ffffff" stroke-miterlimit="10" style={{ mixBlendMode: "normal" }}>
+            <g fill="#ffffff" strokeMiterlimit="10" style={{ mixBlendMode: "normal" }}>
               <g transform="scale(5.33333,5.33333)">
                 <circle cx="14" cy="24" r="12" />
                 <ellipse cx="34" cy="24" rx="6" ry="11" />
