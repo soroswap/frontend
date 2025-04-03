87dab9c47174971a8fd712dd7b3aa4c5740b1a24
esteblock <estebaniglesiasmanriquez@gmail.com>
2025-04-02 21:39:30 -0300

    Use Network Details only if Mounted

--- src/soroban/MySorobanReactProvider.tsx
+++ src/soroban/MySorobanReactProvider.tsx
@@ -1,14 +1,6 @@
-import { NetworkDetails, SorobanReactProvider } from 'stellar-react';
+import { NetworkDetails, SorobanReactProvider, WalletNetwork } from 'stellar-react';
 import useMounted from 'hooks/useMounted';
 
-
-enum WalletNetwork {
-  PUBLIC = "Public Global Stellar Network ; September 2015",
-  TESTNET = "Test SDF Network ; September 2015",
-  FUTURENET = "Test SDF Future Network ; October 2022",
-  SANDBOX = "Local Sandbox Stellar Network ; September 2022",
-  STANDALONE = "Standalone Network ; February 2017"
-}
 export default function MySorobanReactProvider({
   children,
   ...rest
@@ -18,6 +10,10 @@ export default function MySorobanReactProvider({
 }) {
 
 
+  const mounted = useMounted();
+
+  if (!mounted) return null;
+  
   const mainnetNetworkDetails: NetworkDetails = {
     network: WalletNetwork.PUBLIC,
     sorobanRpcUrl: 'https://soroban-rpc.creit.tech/',
@@ -29,15 +25,12 @@ export default function MySorobanReactProvider({
     sorobanRpcUrl: 'https://soroban-testnet.stellar.org/',
     horizonRpcUrl: 'https://horizon-testnet.stellar.org'
   }
-  const mounted = useMounted();
-
-  if (!mounted) return null;
-
+  
   return (
     <SorobanReactProvider
       appName={'Soroswap'}
       allowedNetworkDetails={[mainnetNetworkDetails, testnetNetworkDetails]}
-      activeNetwork={WalletNetwork.TESTNET}
+      activeNetwork={WalletNetwork.PUBLIC}
       {...rest}
     >
       {children}
